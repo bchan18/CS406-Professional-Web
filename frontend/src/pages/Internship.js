b7aafd435c8cd3d4feb70b79e5499c6b2baf0338
bchan18 <114227244+bchan18@users.noreply.github.com>
2024-10-28 15:47:33 -0700

    Updated internship page

--- frontend/src/pages/Internship.js
+++ frontend/src/pages/Internship.js
@@ -6,13 +6,28 @@ function Internship() {
       <h1>Internship</h1>
       <h2>QA Engineering Intern at Power Digital Marketing</h2>
       <h2>Week 1</h2>
-        <p></p>
+        <ul>
+          <li>Onboarding</li>
+          <li>Meeting the Team</li>
+          <li>Learning Asana, Slack, Company</li>
+        </ul>
       <h2>Week 2</h2>
-        <p></p>
+      <ul>
+          <li>Introduction to Manual Testing</li>
+          <li>Shadow QA Tasks</li>
+          <li>Attend Scrum</li>
+          <li>Learn agile work flow</li>
+        </ul>
       <h2>Week 3</h2>
-        <p></p>
+      <ul>
+        <li>Introduction to Selenium, BDD</li>
+        <li>Introduction to Automation Testing</li>
+      </ul>
       <h2>Week 4</h2>
-        <p></p>
+      <ul>
+        <li>Complete QA Tasks independently</li>
+        <li>Report Bugs and assign Tasks to appropiate Team member</li>
+      </ul>
       <h2>Week 5</h2>
         <p></p>
       <h2>Week 6</h2>
